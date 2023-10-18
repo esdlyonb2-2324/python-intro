08811d276d073fccfaf0a5c460667466e675ea56
esdb1 <pierre.lacognata@mail-esd.com>
2023-10-18 13:59:16 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+print("coucou")
+
+dauphin = "voila un dauphin"
+
+print(dauphin)
+
+number = 12
+
+number_float = 12.346578657
+
+#print(dauphin[1])
+
+list = ["truc", "machin", 12, "chaussette"]
+
+for list in range(1, 4):
+    print('coucou')
+
+
+# print(list[1:3])
+
+dictionary = {
+    "name": "luc",
+    "surname": "dupont",
+    "bordel": {
+        "truc": "blablabla"
+    }
+
+}
+
+#print(dictionary["bordel"]["truc"])
+
+truc_tuple = (1, 2)
+
+truc_boolean = True
+
+def fais_des_pates():
+    print("spaghetti")
+
+#print("ravioli")
+
+reponse = input("t'aimes le veau ?")
+
+print("ta réponse est :"+reponse)
+
+print("4"+4)
+
+words = ["bouteille", "huile", "papier-toilette", "beurre"]
+#demander à l'utilisateur un nombre entre 1 et
+# choisir un mot dans le tableau et demander à l'user de le deviner
+#lui dire c'est bon ou pas
